a8049d35ba81d161c951615a632d1dfed6fff116
Emerson Millard <emillard.code@gmail.com>
2022-06-22 12:00:00 -0700

    Added Eureka Client annotation to main class.

--- src/main/java/com/project/microservicesdcb4/MicroservicesDcb4Application.java
+++ src/main/java/com/project/microservicesdcb4/MicroservicesDcb4Application.java
@@ -2,12 +2,16 @@ package com.project.microservicesdcb4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicesDcb4Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MicroservicesDcb4Application.class, args);
+
     }
 
 }
