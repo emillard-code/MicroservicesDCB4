4acbf284669079d8b91946afffbb24e5a64051cd
Emerson Millard <emillard.code@gmail.com>
2022-06-25 12:00:00 -0700

    Added Hystrix annotation to main class.

--- src/main/java/com/project/microservicesdcb4/MicroservicesDcb4Application.java
+++ src/main/java/com/project/microservicesdcb4/MicroservicesDcb4Application.java
@@ -3,9 +3,11 @@ package com.project.microservicesdcb4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class MicroservicesDcb4Application {
 
     public static void main(String[] args) {
