2e5dd2f5719b6ccd381a379f2eff59a1a7718941
Emerson Millard <emillard.code@gmail.com>
2022-06-25 12:00:00 -0700

    Added Hystrix annotation to main class.

--- src/main/java/com/project/MicroservicesDcb4Application.java
+++ src/main/java/com/project/MicroservicesDcb4Application.java
@@ -1,4 +1,4 @@
-package com.project.microservicesdcb4;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
